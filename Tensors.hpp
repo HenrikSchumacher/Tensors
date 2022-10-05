b3fa5e02b895ab83251bc345721777d41d6e36da
HenrikSchumacher <24621470+HenrikSchumacher@users.noreply.github.com>
2022-10-05 19:40:02 +0200

    Header guards

--- Tensors.hpp
+++ Tensors.hpp
@@ -1,45 +1,48 @@
-#pragma once
+#ifndef SCHUMACHER_TENSORS_HPP
+    #define SCHUMACHER_TENSORS_HPP
 
-#include <iostream>
-#include <random>
-#include <cstring>
-#include <array>
-//
-//#define EIGEN_USE_BLAS
-//#define EIGEN_USE_LAPACKE
-//#include <eigen3/Eigen/Dense>
+    #include <iostream>
+    #include <random>
+    #include <cstring>
+    #include <array>
+    //
+    //#define EIGEN_USE_BLAS
+    //#define EIGEN_USE_LAPACKE
+    //#include <eigen3/Eigen/Dense>
 
-#include "Tools/Tools.hpp"
+    #include "Tools/Tools.hpp"
 
-namespace Tensors {
-    
-    using namespace Tools;
-    
-}
+    namespace Tensors {
+        
+        using namespace Tools;
+        
+    }
 
-#include "src/Tensor1.hpp"
-#include "src/Tensor2.hpp"
+    #include "src/Tensor1.hpp"
+    #include "src/Tensor2.hpp"
 
-#include "src/Tensor3.hpp"
-#include "src/ThreadTensor3.hpp"
+    #include "src/Tensor3.hpp"
+    #include "src/ThreadTensor3.hpp"
 
-#include "src/SmallVector.hpp"
-//#include "src/SmallMatrix.hpp"
-#include "src/SmallVectorList.hpp"
-#include "src/SmallSquareMatrix.hpp"
-//#include "src/SmallSymmetricMatrix.hpp"
-#include "src/SmallMatrixList.hpp"
+    #include "src/SmallVector.hpp"
+    //#include "src/SmallMatrix.hpp"
+    #include "src/SmallVectorList.hpp"
+    #include "src/SmallSquareMatrix.hpp"
+    //#include "src/SmallSymmetricMatrix.hpp"
+    #include "src/SmallMatrixList.hpp"
 
-#include "src/TwoArrayQuickSort.hpp"
-#include "src/TimSort.hpp"
+    #include "src/TwoArrayQuickSort.hpp"
+    #include "src/TimSort.hpp"
 
-#include "src/DenseBLAS.hpp"
+    #include "src/DenseBLAS.hpp"
 
-#include "src/SparseBLAS.hpp"
-#include "src/SparseCSR.hpp"
-#include "src/SparseBinaryMatrixCSR.hpp"
-#include "src/SparseBinaryMatrixVBSR.hpp"
-#include "src/SparseMatrixCSR.hpp"
+    #include "src/SparseBLAS.hpp"
+    #include "src/SparseCSR.hpp"
+    #include "src/SparseBinaryMatrixCSR.hpp"
+    #include "src/SparseBinaryMatrixVBSR.hpp"
+    #include "src/SparseMatrixCSR.hpp"
 
-#include "src/SparseBlockBLAS.hpp"
-#include "src/SparseMatrixBSR.hpp"
+    #include "src/SparseBlockBLAS.hpp"
+    #include "src/SparseMatrixBSR.hpp"
+
+#endif
