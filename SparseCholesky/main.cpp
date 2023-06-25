afa112a3d64d4e202782912721f26e59ad830af5
HenrikSchumacher <24621470+HenrikSchumacher@users.noreply.github.com>
2023-06-25 14:49:54 +0200

    Added test code for Accelerate.

--- SparseCholesky/main.cpp
+++ SparseCholesky/main.cpp
@@ -16,98 +16,29 @@
 #include "MyBLAS.hpp"
 #include "Sparse.hpp"
 
-//#include "../src/Sparse/Metis_Wrapper.hpp"
+#include "../src/Sparse/Metis.hpp"
 
+#include "../src/CHOLMOD/CholeskyDecomposition.hpp"
+#include "../src/CHOLMOD/ApproximateMinimumDegree.hpp"
+#include "../src/CHOLMOD/NestedDissection.hpp"
 
 using namespace Tools;
 using namespace Tensors;
 
 using Scal   = double;
 using Real   = Scalar::Real<Scal>;
-using LInt   = int64_t;
+//using LInt   = int64_t;
+//using LInt   = int64_t;
+//using Int    = int64_t;
+
+using LInt   = long;
 using Int    = int32_t;
 
 int main(int argc, const char * argv[])
 {
 //    print("Hello world!");
-    constexpr Int thread_count = 1;
+    constexpr Int thread_count = 8;
     constexpr Int tree_top_depth = 5;
-
-//    dump(openblas_get_num_threads());
-//    openblas_set_num_threads(1);
-//    dump(openblas_get_num_threads());
-    
-//    const Int n = 49;
-//
-//    Int perm_array [49] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
-//        19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35,
-//        36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48};
-////
-////    Int perm [49] = {13, 10, 3, 0, 17, 6, 20, 18, 11, 2, 16, 14, 7, 5, 12, 1, 19, 4, 8,
-////    9, 15, 34, 31, 24, 21, 38, 27, 41, 39, 32, 23, 37, 35, 28, 26, 33,
-////        22, 40, 25, 29, 30, 36, 48, 44, 46, 45, 43, 47, 42};
-//
-////    long long perm [49] = {28, 5, 16, 6, 42, 36, 22, 33, 3, 11, 23, 24, 46, 26, 21, 31, 14, 9, \
-////        0, 34, 12, 45, 18, 1, 44, 2, 47, 29, 20, 4, 43, 35, 19, 41, 15, 13, \
-////        32, 38, 10, 48, 37, 40, 8, 30, 25, 39, 27, 17, 7};
-//
-//
-//    LInt rp [50] = {0, 4, 10, 16, 22, 31, 40, 46, 55, 64, 70, 74, 80, 89, 95, 104, 113,
-//        122, 128, 134, 143, 152, 158, 167, 176, 180, 186, 192, 198, 207, 216,
-//        225, 231, 240, 246, 250, 256, 265, 274, 280, 289, 298, 304, 310, 319,
-//        328, 337, 346, 355, 361};
-//
-//    Int ci [361] = {0, 2, 6, 7, 1, 2, 7, 8, 18, 19, 2, 0, 1, 6, 7, 8, 3, 5, 6, 7, 42,
-//        43, 4, 5, 7, 8, 19, 20, 43, 44, 45, 5, 3, 4, 6, 7, 8, 42, 43, 44, 6,
-//        0, 2, 3, 5, 7, 7, 0, 1, 2, 3, 4, 5, 6, 8, 8, 1, 2, 4, 5, 7, 18, 19,
-//        20, 9, 11, 15, 16, 18, 19, 10, 11, 16, 17, 11, 9, 10, 15, 16, 17, 12,
-//        14, 15, 16, 19, 20, 45, 46, 47, 13, 14, 16, 17, 47, 48, 14, 12, 13,
-//        15, 16, 17, 46, 47, 48, 15, 9, 11, 12, 14, 16, 18, 19, 20, 16, 9, 10,
-//        11, 12, 13, 14, 15, 17, 17, 10, 11, 13, 14, 16, 18, 1, 8, 9, 15, 19,
-//        19, 1, 4, 8, 9, 12, 15, 18, 20, 20, 4, 8, 12, 15, 19, 44, 45, 46, 21,
-//        23, 27, 28, 42, 43, 22, 23, 28, 29, 39, 40, 43, 44, 45, 23, 21, 22,
-//        27, 28, 29, 42, 43, 44, 24, 26, 27, 28, 25, 26, 28, 29, 40, 41, 26,
-//        24, 25, 27, 28, 29, 27, 21, 23, 24, 26, 28, 28, 21, 22, 23, 24, 25,
-//        26, 27, 29, 29, 22, 23, 25, 26, 28, 39, 40, 41, 30, 32, 36, 37, 39,
-//        40, 45, 46, 47, 31, 32, 37, 38, 47, 48, 32, 30, 31, 36, 37, 38, 46,
-//        47, 48, 33, 35, 36, 37, 40, 41, 34, 35, 37, 38, 35, 33, 34, 36, 37,
-//        38, 36, 30, 32, 33, 35, 37, 39, 40, 41, 37, 30, 31, 32, 33, 34, 35,
-//        36, 38, 38, 31, 32, 34, 35, 37, 39, 22, 29, 30, 36, 40, 44, 45, 46,
-//        40, 22, 25, 29, 30, 33, 36, 39, 41, 41, 25, 29, 33, 36, 40, 42, 3, 5,
-//        21, 23, 43, 43, 3, 4, 5, 21, 22, 23, 42, 44, 44, 4, 5, 20, 22, 23,
-//        39, 43, 45, 45, 4, 12, 20, 22, 30, 39, 44, 46, 46, 12, 14, 20, 30,
-//        32, 39, 45, 47, 47, 12, 13, 14, 30, 31, 32, 46, 48, 48, 13, 14, 31,
-//        32, 47};
-//
-//    Scal a [361] = {4., -1., -1., -1., 6., -1., -1., -1., -1., -1., 6., -1., -1., -1.,
-//        -1., -1., 6., -1., -1., -1., -1., -1., 9., -1., -1., -1., -1., -1.,
-//        -1., -1., -1., 9., -1., -1., -1., -1., -1., -1., -1., -1., 6., -1.,
-//        -1., -1., -1., -1., 9., -1., -1., -1., -1., -1., -1., -1., -1., 9.,
-//        -1., -1., -1., -1., -1., -1., -1., -1., 6., -1., -1., -1., -1., -1.,
-//        4., -1., -1., -1., 6., -1., -1., -1., -1., -1., 9., -1., -1., -1.,
-//        -1., -1., -1., -1., -1., 6., -1., -1., -1., -1., -1., 9., -1., -1.,
-//        -1., -1., -1., -1., -1., -1., 9., -1., -1., -1., -1., -1., -1., -1.,
-//        -1., 9., -1., -1., -1., -1., -1., -1., -1., -1., 6., -1., -1., -1.,
-//        -1., -1., 6., -1., -1., -1., -1., -1., 9., -1., -1., -1., -1., -1.,
-//        -1., -1., -1., 9., -1., -1., -1., -1., -1., -1., -1., -1., 6., -1.,
-//        -1., -1., -1., -1., 9., -1., -1., -1., -1., -1., -1., -1., -1., 9.,
-//        -1., -1., -1., -1., -1., -1., -1., -1., 4., -1., -1., -1., 6., -1.,
-//        -1., -1., -1., -1., 6., -1., -1., -1., -1., -1., 6., -1., -1., -1.,
-//        -1., -1., 9., -1., -1., -1., -1., -1., -1., -1., -1., 9., -1., -1.,
-//        -1., -1., -1., -1., -1., -1., 9., -1., -1., -1., -1., -1., -1., -1.,
-//        -1., 6., -1., -1., -1., -1., -1., 9., -1., -1., -1., -1., -1., -1.,
-//        -1., -1., 6., -1., -1., -1., -1., -1., 4., -1., -1., -1., 6., -1.,
-//        -1., -1., -1., -1., 9., -1., -1., -1., -1., -1., -1., -1., -1., 9.,
-//        -1., -1., -1., -1., -1., -1., -1., -1., 6., -1., -1., -1., -1., -1.,
-//        9., -1., -1., -1., -1., -1., -1., -1., -1., 9., -1., -1., -1., -1.,
-//        -1., -1., -1., -1., 6., -1., -1., -1., -1., -1., 6., -1., -1., -1.,
-//        -1., -1., 9., -1., -1., -1., -1., -1., -1., -1., -1., 9., -1., -1.,
-//        -1., -1., -1., -1., -1., -1., 9., -1., -1., -1., -1., -1., -1., -1.,
-//        -1., 9., -1., -1., -1., -1., -1., -1., -1., -1., 9., -1., -1., -1.,
-//        -1., -1., -1., -1., -1., 6., -1., -1., -1., -1., -1.};
-    
-    using namespace Tensors;
-    using namespace Tools;
     
     const char * homedir = getenv("HOME");
 
@@ -115,9 +46,11 @@ int main(int argc, const char * argv[])
     {
         homedir = getpwuid(getuid())->pw_dir;
     }
-    std::string filename ( homedir );
+    std::string path ( homedir );
     
-    filename += "/github/Tensors/SparseMatrices/Spot_0.txt";
+    Profiler::Clear( path );
+    
+    std::string filename = path + "/github/Tensors/SparseMatrices/Spot_0.txt";
     
     const Int nrhs = 3;
     
@@ -139,23 +72,39 @@ int main(int argc, const char * argv[])
     
 //    Permutation<Int> perm ( perm_array, n, Inverse::False, thread_count );
     
-    Permutation<Int> perm ( n, thread_count );
+//    Permutation<Int> perm ( n, thread_count );
     
 //    Metis_Wrapper()( &rp[0], &ci[0], perm );
     
-//    tic("Metis_Wrapper");
-//    Metis_Wrapper()( A.Outer().data(), A.Inner().data(), perm );
-//    toc("Metis_Wrapper");
+//    ptic("Metis");
+//    Metis()( A.Outer().data(), A.Inner().data(), perm );
+//    ptoc("Metis");
 //    print( perm.GetPermutation().ToString() );
     
+//    tic("CHOLMOD::ApproximateMinimumDegree");
+//    Permutation<Int> perm = CHOLMOD::ApproximateMinimumDegree<Int>()(
+//        A.Outer().data(), A.Inner().data(), n, thread_count
+//    );
+//    toc("CHOLMOD::ApproximateMinimumDegree");
+    
+    tic("CHOLMOD::NestedDissection");
+    Permutation<Int> perm = CHOLMOD::NestedDissection<int>()(
+        A.Outer().data(), A.Inner().data(), n, thread_count
+    );
+    toc("CHOLMOD::NestedDissection");
 
-    Sparse::CholeskyDecomposition<Scal, Int, LInt> chol ( A.Outer().data(), A.Inner().data(), std::move(perm), tree_top_depth );
+
+    Sparse::CholeskyDecomposition<Scal,Int,LInt> S ( A.Outer().data(), A.Inner().data(), std::move(perm), tree_top_depth );
+
+    tic("My Cholesky factorization");
+    S.SymbolicFactorization();
+
+    S.NumericFactorization(A.Values().data(), reg);
+    toc("My Cholesky factorization");
     
-    chol.SN_SymbolicFactorization();
-    
-    chol.SN_NumericFactorization(A.Values().data(), reg);
-    
-    chol.Solve(B.data(), X.data(), nrhs);
+    tic("My Cholesky solve");
+    S.Solve(B.data(), X.data(), nrhs);
+    toc("My Cholesky solve");
     
     Y = B;
     
@@ -167,22 +116,123 @@ int main(int argc, const char * argv[])
     dump(Y.MaxNorm());
     
     
+//    CHOLMOD::CholeskyDecomposition<Scal,Int,LInt> cholmod ( A.Outer().data(), A.Inner().data(), n );
+//
+//    cholmod.SymbolicFactorization();
+//
+//    cholmod.NumericFactorization(A.Values().data());
+//
+//    cholmod.Solve(B.data(), X.data(), nrhs);
+//
+//    Y = B;
+//
+//    A.Dot(Scal(-1), X, Scal(1), Y);
+//
+//    dump(Y.MaxNorm());
+    
 //    print(ToString(Y));
-
+//
 //    {
 //        Tensor2<Real,Int> ZZ  (3,3, 2.);
-//        
+//
 //        print( ToString( ZZ.data(), {3,3} ) );
 //    }
-//    
-//    
+//
+//
 //    {
 //        Tensor1<Real,Int> ZZ  (1*2*3*4, 4.);
-//        
+//
 //        print( ToString( ZZ.data(), {1,2,3,4} ) );
-//    
+//
 //    }
+//
     
+
+    
+    {
+        SparseMatrixStructure pat = {
+            A.RowCount(), A.ColCount(), A.Outer().data(), A.Inner().data(),
+            SparseAttributes_t{
+                false, SparseUpperTriangle, SparseSymmetric, 0, false
+            },
+            1
+        };
+        
+        SparseMatrix_Double AA = { pat, A.Values().data() };
+        
+        Tensor2<Scal,Int> BT ( B.Dimension(1), B.Dimension(0));
+//        Tensor2<Scal,Int> XT ( X.Dimension(1), X.Dimension(0));
+        
+        BT.ReadTransposed(B.data());
+        
+//        unc SparseFactor(
+//            _ type: SparseFactorization_t,
+//            _ Matrix: SparseMatrixStructure
+//        ) -> SparseOpaqueSymbolicFactorization
+
+        tic("Accelerate symbolic factorization");
+        SparseOpaqueSymbolicFactorization Lsym = SparseFactor( SparseFactorizationCholesky, pat );
+        toc("Accelerate symbolic factorization");
+        
+//        int * bla = reinterpret_cast<int*>(Lsym.factorization) + 46;
+//        
+//        int dims [1] = {n};
+//        
+//        print( ArrayToString(bla, &dims[0], 1) );
+        
+        tic("Accelerate numeric factorization");
+        SparseOpaqueFactorization_Double L = SparseFactor( Lsym, AA );
+        toc("Accelerate numeric factorization");
+        
+        tic("Accelerate Cholesky solve");
+        SparseSolve(
+            L,
+            DenseMatrix_Double{ B.Dimension(0), B.Dimension(1), B.Dimension(0), SparseAttributes_t(), BT.data() }
+        );
+        toc("Accelerate Cholesky solve");
+        
+        BT.WriteTransposed(X.data());
+        
+        Y = B;
+
+        A.Dot(Scal(-1), X, Scal(1), Y);
+
+        dump(Y.MaxNorm());
+    }
     
     return 0;
 }
+
+
+
+//SparseMatrix_Double A = {
+//       SparseMatrixStructure {
+//             n, n, reinterpret_cast<long>(rp.data()), reinterpret_cast<long>(ci.data()),
+//             SparseAttributes_t {
+//                   false, SparseUpperTriangle, SparseSymmetric, 0, false
+//               },
+//             1
+//         } ,
+//       a . data ()
+//   };
+//
+//Tensor2 < Scal, int > BT ( nrhs, n);
+//
+//BT . ReadTransposed (B . data ());
+//
+//tic (\" Accelerate Cholesky factorization \");
+//SparseOpaqueFactorization_Double L =
+//  SparseFactor ( SparseFactorizationCholesky, A );
+//toc (\" Accelerate Cholesky factorization \");
+//
+//tic (\" Accelerate Cholesky solve \");
+//SparseSolve (
+//       L,
+//       DenseMatrix_Double { n, nrhs, n, SparseAttributes_t (),
+//     BT . data () }
+//   );
+//toc (\" Accelerate Cholesky solve \");
+//
+//auto X = mma::makeMatrix < mreal > ( n, nrhs );
+//
+//BT . WriteTransposed (X . data ());
