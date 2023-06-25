f4e5078a08c308637827c8dd115f83f75b00ce40
HenrikSchumacher <24621470+HenrikSchumacher@users.noreply.github.com>
2023-06-25 14:45:51 +0200

    Removed _Wrapper suffix.

---
+++ src/LAPACK.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+namespace Tensors
+{
+    namespace LAPACK
+    {
+        constexpr int to_LAPACK( Layout layout )
+        {
+            if ( layout == Layout::RowMajor )
+            {
+                return 101; // LAPACK_ROW_MAJOR
+            }
+            else // if ( layout == Layout::RowMajor )
+            {
+                return 102; // LAPACK_COL_MAJOR
+            }
+        }
+        
+        constexpr char to_LAPACK( Op op )
+        {
+            if ( op == Op::Id )
+            {
+                return 'N';
+            }
+            else if ( op == Op::Trans )
+            {
+                return 'T';
+            }
+            else // if ( op == Op::ConjTranspose )
+            {
+                return 'C';
+            }
+        }
+        
+        constexpr char to_LAPACK( UpLo uplo )
+        {
+            if ( uplo == UpLo::Upper )
+            {
+                return 'U';
+            }
+            else // if ( uplo == UpLo::Lower )
+            {
+                return 'L';
+            }
+        }
+        
+    }
+}
+
+#include "LAPACK/potrf.hpp"
+
