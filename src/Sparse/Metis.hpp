a123db656d9f53eb9ed2ba6719835a91bdc12050
HenrikSchumacher <24621470+HenrikSchumacher@users.noreply.github.com>
2024-02-17 21:21:40 -0500

    Maintenance.

--- src/Sparse/Metis.hpp
+++ src/Sparse/Metis.hpp
@@ -26,23 +26,16 @@ namespace Tensors
             mptr<I_1> rp_, mptr<I_2> ci_, const I_3 n_, const I_4 final_thread_count = 1
         )
         {
-            Int opts [METIS_NOPTIONS] = {};
+//            Int opts [METIS_NOPTIONS] = {};
 
-            METIS_SetDefaultOptions(&opts[0]);
+//            METIS_SetDefaultOptions(&opts[0]);
             
-            opts[METIS_OPTION_NUMBERING] = 0;
-            
-//            opts[METIS_OPTION_SEED]      = std::random_device()();
-            opts[METIS_OPTION_SEED]      = 0;
-            
-//            opts[METIS_OPTION_CCORDER]   = 0;
-            
-//            opts[METIS_OPTION_DBGLVL]    = 256;
+//            opts[METIS_OPTION_NUMBERING] = 0;
             
             Int n = static_cast<Int>(n_);
             
-            Tensor1<Int,Int> rp    ( n + 1 );
-            Tensor1<Int,Int> ci    ( rp[n] );
+            Tensor1<Int,Int> rp    ( n + 1  );
+            Tensor1<Int,Int> ci    ( rp_[n] );
             Tensor1<Int,Int> perm  ( n );
             Tensor1<Int,Int> iperm ( n );
             
@@ -63,20 +56,20 @@ namespace Tensors
                     if( i != j )
                     {
                         ci[nnz_counter] = j;
+                        ++nnz_counter;
                     }
                 }
                 
                 rp[i+1] = nnz_counter;
             }
             
-            
             ptic("METIS_NodeND");
-            METIS_NodeND(
-                &n, rp.data(), ci.data(), nullptr, &opts[0], perm.data(), iperm.data()
+            (void)METIS_NodeND(
+                &n, rp.data(), ci.data(), nullptr, nullptr, perm.data(), iperm.data()
             );
             ptoc("METIS_NodeND");
             
-            return Permutation<I_0>( perm.data(), n, Inverse::False, final_thread_count );
+            return Permutation<I_0>( perm.data(), n, Inverse::False, I_0(1) );
         }
     }; // class Metis
     
