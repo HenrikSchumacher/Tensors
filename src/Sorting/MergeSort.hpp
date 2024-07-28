91bc7044f9dbfccc26339c72ed6f686a6eaaa61c
HenrikSchumacher <24621470+HenrikSchumacher@users.noreply.github.com>
2024-07-28 16:01:02 +0200

    Adding template keyword.

--- src/Sorting/MergeSort.hpp
+++ src/Sorting/MergeSort.hpp
@@ -241,8 +241,8 @@ namespace Tensors
             last_row_begin      = (Size_T(1) << Depth(node_count-1)) - 1;
             offset              = node_count - interior_node_count - last_row_begin;
             
-            node_begin.RequireSize<false>( node_count );
-            node_end  .RequireSize<false>( node_count );
+            node_begin.template RequireSize<false>( node_count );
+            node_end  .template RequireSize<false>( node_count );
 
             // TODO: Can we somehow avoid the use of node_begin and node_end?
             
