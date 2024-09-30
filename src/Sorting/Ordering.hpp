17748eff3e8fde624d46912520e09c6df249f901
HenrikSchumacher <24621470+HenrikSchumacher@users.noreply.github.com>
2024-09-30 19:29:48 -0400

    Added ordering with stride.

--- src/Sorting/Ordering.hpp
+++ src/Sorting/Ordering.hpp
@@ -30,6 +30,35 @@ namespace Tensors
             }
         );
     }
+    
+    template<
+        Size_T N = VarSize, typename C = std::less<>,
+        typename T, typename Int
+    >
+    void Ordering( cptr<T> key, Int stride, mptr<Int> perm, Int n, C comp = C() )
+    {
+        static_assert(IntQ<Int>,"");
+        
+        if( n <= Scalar::Zero<Int> )
+        {
+            return;
+        }
+        
+        if( n == Scalar::One<Int> )
+        {
+            perm[0] = 0;
+            return;
+        }
+        
+        iota_buffer<N,Sequential>( perm, n );
+        
+        Sort( perm, &perm[n],
+            [=]( const Int i, const Int j ) -> bool
+            {
+                return comp( key[stride * i], key[stride * j] );
+            }
+        );
+    }
 
 }
 
