e65eede3f51ff571beda4e0017a507b0bcb50632
HenrikSchumacher <24621470+HenrikSchumacher@users.noreply.github.com>
2025-09-11 20:51:45 +0200

    Added nonzero default element in Sparse::Vector; debugging of Sparse::VectorPartialDot.

--- src/Sparse/Vector.hpp
+++ src/Sparse/Vector.hpp
@@ -20,19 +20,22 @@ namespace Tensors
             Int thread_count = 1;
             Tensor1<Int ,Int> idx;  // Needs to be sorted!
             Tensor1<Scal,Int> val;
+            Scal default_value = 0;
             
         public:
             
-            template<typename I_0, typename I_1, typename I_2>
+            template<typename I_0, typename I_1, typename I_2, typename S>
             Vector(
-                   const I_0 n_,
-                   const I_1 nnz_,
-                   const I_2 thread_count_
-                   )
-            :   n            { int_cast<Int>(n_)            }
-            ,   idx          { int_cast<Int>(nnz_)          }
-            ,   val          { int_cast<Int>(nnz_)          }
-            ,   thread_count { int_cast<Int>(thread_count_) }
+               const I_0 n_,
+               const I_1 nnz_,
+               const I_2 thread_count_  = 1,
+               const S   default_value_ = 0
+            )
+            :   n             { int_cast<Int>(n_)                 }
+            ,   idx           { int_cast<Int>(nnz_)               }
+            ,   val           { int_cast<Int>(nnz_)               }
+            ,   thread_count  { int_cast<Int>(thread_count_)      }
+            ,   default_value { static_cast<Scal>(default_value_) }
             {
                 static_assert(IntQ<I_0>,"");
                 static_assert(IntQ<I_1>,"");
@@ -41,16 +44,18 @@ namespace Tensors
             
             template<typename S, typename J_0, typename I_0, typename I_1, typename I_2>
             Vector(
-                   const J_0 * const idx_,  // Needs t be sorted!
-                   const S   * const val_,
-                   const I_0 n_,
-                   const I_1 nnz_,
-                   const I_2 thread_count_
-                   )
+                const J_0 * const idx_,  // Needs t be sorted!
+                const S   * const val_,
+                const I_0 n_,
+                const I_1 nnz_,
+                const I_2 thread_count_  = 1,
+                const S   default_value_ = 0
+            )
             :   n    { int_cast<Int>(n_)         }
             ,   idx  { idx_, int_cast<Int>(nnz_) }
             ,   val  { val_, int_cast<Int>(nnz_) }
             ,   thread_count { int_cast<Int>(thread_count_) }
+            ,   default_value { static_cast<Scal>(default_value_) }
             {
                 static_assert(ArithmeticQ<S>,"");
                 static_assert(IntQ<I_0>,"");
@@ -90,9 +95,14 @@ namespace Tensors
                 return n;
             }
             
-            Int NonzeroCount() const
+            Int ExplicitCount() const
             {
                 return idx.Size();
+            }
+            
+            Scal DefaultValue() const
+            {
+                return default_value;
             }
             
             mref<Tensor1<Scal,Int>> Indices()
@@ -164,6 +174,8 @@ namespace Tensors
             
         }; // Vector
         
+        
+        
         /*!@brief This effectively reads odd the portions of the sparse vectors `u` and `v` given by `{u_i_begin,...,u_i_end-1}` and `{ v_i_begin,...,v_i_end-1}` and computes their dot product without doing any actual copy.
          * Binary search (`FindRange`) is used to find the range in `u_idx` and `v_idx` that correspond to the indices `{u_i_begin,...,u_i_end-1}` and `{v_i_begin,...,v_i_end-1}`, so `u_idx` and `v_idx` are required to be sorted.
          */
@@ -176,126 +188,230 @@ namespace Tensors
         static Scal VectorPartialDot(
             cptr<u_Int>  u_idx,
             cptr<u_Scal> u_val,
+            const u_Scal u_default,
             const u_Int  u_n,
             const u_Int  u_nnz,
             const u_Int  u_i_begin,
             const u_Int  u_i_end,
             cptr<v_Int>  v_idx,
             cptr<v_Scal> v_val,
+            const u_Scal v_default,
             const v_Int  v_n,
             const v_Int  v_nnz,
             const v_Int  v_i_begin,
             const v_Int  v_i_end
         )
         {
-            Scal sum = 0;
+            if( u_i_begin >= u_i_end ) { return 0; }
+            if( v_i_begin >= v_i_end ) { return 0; }
             
-            if( u_i_begin > u_i_end ) { return sum; }
-            if( v_i_begin > v_i_end ) { return sum; }
-            
-            if( u_i_begin < u_Int(0) )
-            {
-                eprint(std::string("Sparse::VectorPartialDot")+": u_i_begin < 0.");
-                return sum;
-            }
-            if( u_i_end > u_n )
-            {
-                eprint(std::string("Sparse::VectorPartialDot")+": u_i_end > u_n.");
-                return sum;
-            }
-            
-            if( v_i_begin < v_Int(0) )
-            {
-                eprint(std::string("Sparse::VectorPartialDot")+": v_i_begin  < 0");
-                return sum;
-            }
-            if( v_i_end > v_n )
-            {
-                eprint(std::string("Sparse::VectorPartialDot")+": v_i_end > v_n.");
-                return sum;
-            }
-            
-            if( std::cmp_not_equal( u_i_end - u_i_begin, v_i_end - v_i_begin) )
+            if( std::cmp_not_equal(u_i_end - u_i_begin,v_i_end - v_i_begin) )
             {
                 eprint(std::string("Sparse::VectorPartialDot")+": u_i_end - u_i_begin != v_i_end - v_i_begin.");
-                return sum;
+                return 0;
             }
             
+//            TOOLS_DUMP(u_idx);
+//            TOOLS_DUMP(u_val);
+//            TOOLS_DUMP(u_nnz);
+//            TOOLS_DUMP(v_idx);
+//            TOOLS_DUMP(v_val);
+//            TOOLS_DUMP(v_nnz);
+            
+//            TOOLS_DUMP(u_i_begin);
+//            TOOLS_DUMP(u_i_end  );
+//            TOOLS_DUMP(v_i_begin);
+//            TOOLS_DUMP(v_i_end  );
+//            
+//            TOOLS_DUMP(u_default);
+//            TOOLS_DUMP(v_default);
+//            
+//            TOOLS_DUMP(u_nnz);
+//            TOOLS_DUMP(v_nnz);
+//            
+//            TOOLS_DUMP(u_idx);
+//            TOOLS_DUMP(u_val);
+//            
+//            TOOLS_DUMP(v_idx);
+//            TOOLS_DUMP(v_val);
+            
             // TODO: Check cost here if u_i_begin == 0 and u_i_end == u_n.
-            auto [u_p_begin,u_p_end] = FindRange(u_idx,u_nnz,u_i_begin,u_i_end);
+            auto [u_p_begin,u_p_end] = ( u_idx == nullptr ) || ( u_val == nullptr )
+                                     ? std::pair{u_nnz,u_nnz}
+                                     : FindRange(
+                                           u_idx,u_nnz,u_i_begin,u_i_end
+                                       );
+            
+//            TOOLS_DUMP(u_p_begin);
+//            TOOLS_DUMP(u_p_end);
             
             // TODO: Check cost here if v_i_begin == 0 and v_i_end == v_n.
-            auto [v_p_begin,v_p_end] = FindRange(v_idx,v_nnz,v_i_begin,v_i_end);
+            auto [v_p_begin,v_p_end] = ( v_idx == nullptr ) || ( v_val == nullptr )
+                                     ? std::pair{v_nnz,v_nnz}
+                                     : FindRange(
+                                           v_idx,v_nnz,v_i_begin,v_i_end
+                                       );
             
-            // One would think that one could use Details::Dot from here on, but we have to apply a shift of the indices in order to test for equality.
+//            TOOLS_DUMP(v_p_begin);
+//            TOOLS_DUMP(v_p_end);
             
+            // Caution: One might be tempted to use Details::Dot from here on, but we have to apply a shift of the indices in order to test for equality. Also Details::Dot does not account for nonzero default values.
+
+            const Scal u_def = static_cast<Scal>(u_default);
+            const Scal v_def = static_cast<Scal>(v_default);
+            
+            const bool u_defQ = (u_def != Scal(0));
+            const bool v_defQ = (v_def != Scal(0));
+            
+            Scal   sum = 0;
+            Size_T uv_counter = 0; // Counts the number of explicit position matches.
+
             // Pointers into u_idx and v_idx.
             u_Int u_p = u_p_begin;
             v_Int v_p = v_p_begin;
             
-            if ( u_p >= u_p_end ) { return sum; }
-            // This automatically corrects non-0-basedness!
-            u_Int u_i = u_idx[u_p] - u_i_begin;
+            // Caution: We must not terminate here if u_p >= u_p_end or v_p >= v_p_end as this won't treat nonzero default values correctly.
             
-            if ( v_p >= v_p_end ) { return sum; }
-            // This automatically corrects non-0-basedness!
-            v_Int v_i = v_idx[v_p] - v_i_begin;
-            
-            
-            auto move_u_forward = [&u_p,&u_i,u_idx,u_i_begin,u_p_end]() -> bool
+            if( (u_p < u_p_end) && (v_p < v_p_end) )
             {
-                if ( ++u_p >= u_p_end ) { return false; }
-                // This automatically corrects non-0-basedness!
-                u_i = u_idx[u_p] - u_i_begin;
-                return true;
-            };
-            
-            auto move_v_forward = [&v_p,&v_i,v_idx,v_i_begin,v_p_end]() -> bool
-            {
-                if ( ++v_p >= v_p_end ) { return false; }
-                // This automatically corrects non-0-basedness!
-                v_i = v_idx[v_p] - v_i_begin;
-                return true;
-            };
-            
-            while( true )
-            {
-                while ( u_i < v_i )
+                u_Int u_i = u_idx[u_p] - u_i_begin; // Corrects non-0-basedness!
+                v_Int v_i = v_idx[v_p] - v_i_begin; // Corrects non-0-basedness!
+
+                while( true )
                 {
-                    if( !move_u_forward() ) { return sum; }
-                }
-                // If we have not terminated, yet, then we have u_i >= v_i now.
-                
-                while ( v_i < u_i )
-                {
-                    if( !move_v_forward() ) { return sum; }
-                }
-                // If we have not terminated, yet, then we have v_i >= u_i now.
-                
-                if ( u_i == v_i )
-                {
-                    sum += static_cast<Scal>(u_val[u_p]) * static_cast<Scal>(v_val[v_p]);
-                    
-                    if( !move_u_forward() ) { return sum; }
-                    if( !move_v_forward() ) { return sum; }
-                }
-                else
-                {
-                    // We actually can reach this point, i.e., if
-                    // u_idx = {0,2,4} and v_idx = {1,2,5}.
-                    
                     if ( u_i < v_i )
                     {
-                        if( !move_u_forward() ) { return sum; }
+                        // We put an if-check here to give branch prediction a chance for optimization. (Also, v_defQ is marked const, so maybe the compiler creates appropriate branches at compile time.)
+                        if ( v_defQ )
+                        {
+                            sum += static_cast<Scal>(u_val[u_p]) * v_def;
+                        }
+                        ++u_p;
+                        if ( u_p >= u_p_end ) { break; }
+                        u_i = u_idx[u_p] - u_i_begin; // Corrects non-0-basedness!
                     }
-                    else // if ( u_i > v_i )
+                    else if ( u_i > v_i )
                     {
-                        if( !move_v_forward() ) { return sum; }
+                        // We put an if-check here to give branch prediction a chance for optimization. (Also, u_defQ is marked const, so maybe the compiler creates appropriate branches at compile time.)
+                        if ( u_defQ )
+                        {
+                            sum += u_def * static_cast<Scal>(v_val[v_p]);
+                        }
+                        ++v_p;
+                        if ( v_p >= v_p_end ) { break; }
+                        v_i = v_idx[v_p] - v_i_begin; // Corrects non-0-basedness!
+                    }
+                    else // if ( u_i == v_i )
+                    {
+                        ++uv_counter;
+                        sum += static_cast<Scal>(u_val[u_p]) * static_cast<Scal>(v_val[v_p]);
+                        
+                        ++u_p;
+                        ++v_p;
+                        if ( (u_p >= u_p_end) || (v_p >= v_p_end) ) { break; }
+                        u_i = u_idx[u_p] - u_i_begin; // Corrects non-0-basedness!
+                        v_i = v_idx[v_p] - v_i_begin; // Corrects non-0-basedness!
                     }
                 }
             }
             
+            // If v_def != 0, then we have to handle the unvisited explicit values of u.
+            if ( v_defQ )
+            {
+                while ( u_p < u_p_end )
+                {
+                    sum += static_cast<Scal>(u_val[u_p]) * v_def;
+                    ++u_p;
+                }
+            }
+            
+            // If u_def != 0, then we have to handle the unvisited explicit values of v.
+            if ( u_defQ )
+            {
+                while ( v_p < v_p_end )
+                {
+                    sum += u_def * static_cast<Scal>(v_val[v_p]);
+                    ++v_p;
+                }
+            }
+            
+            if ( u_defQ && v_defQ )
+            {
+                const Size_T u_i_count = ToSize_T(u_i_end - u_i_begin);
+//                const Size_T v_i_count = ToSize_T(v_i_end - v_i_begin);
+                
+                const Size_T u_p_count = ToSize_T(u_p_end - u_p_begin);
+                const Size_T v_p_count = ToSize_T(v_p_end - v_p_begin);
+                
+//                // The number of cases where an explicit position of u was not matched by an explicit position of v.
+//                const Size_T u_p_unmatched_count = u_p_count - uv_counter;
+                
+                // The number of cases where an explicit position of v was not matched by an explicit position of u.
+                const Size_T v_p_unmatched_count = v_p_count - uv_counter;
+                
+//                TOOLS_DUMP(sum);
+                
+                const Size_T u_def_pair_count = u_i_count - u_p_count - v_p_unmatched_count;
+//                const Size_T v_def_pair_count = v_i_count - v_p_count - u_p_unmatched_count;
+//                
+//                // DEBUGGING
+//                if( u_def_pair_count != v_def_pair_count )
+//                {
+//                    eprint("u_def_pair_count != v_def_pair_count");
+//                    
+//                    TOOLS_DUMP(u_def_pair_count);
+//                    TOOLS_DUMP(v_def_pair_count);
+//                }
+                
+//                TOOLS_DUMP(u_def_pair_count);
+                
+                sum += u_def * v_def * static_cast<Scal>(u_def_pair_count);
+            }
+            
             return sum;
+        }
+        
+        
+        template<
+            typename u_Scal, typename Int, typename Scal = u_Scal
+        >
+        static Scal VectorPartialSum(
+            cptr<Int>    u_idx,
+            cptr<u_Scal> u_val,
+            const u_Scal u_default,
+            const Int    u_n,
+            const Int    u_nnz,
+            const Int    u_i_begin,
+            const Int    u_i_end
+        )
+        {
+            Int  * v_idx = nullptr;
+            Scal * v_val = nullptr;
+            
+            return VectorPartialDot<u_Scal,Int,Scal,Int,Scal>(
+                u_idx, u_val, u_default, u_n, u_nnz , u_i_begin, u_i_end,
+                v_idx, v_val, Scal(1)  , u_n, Int(0), u_i_begin, u_i_end
+            );
+        }
+        
+        template<
+            typename u_Scal, typename Int, typename Scal = u_Scal
+        >
+        static Scal VectorTotal(
+            cptr<Int>    u_idx,
+            cptr<u_Scal> u_val,
+            const u_Scal u_default,
+            const Int    u_n,
+            const Int    u_nnz
+        )
+        {
+            Int  * v_idx = nullptr;
+            Scal * v_val = nullptr;
+            
+            return VectorPartialDot<u_Scal,Int,Scal,Int,Scal>(
+                u_idx, u_val, u_default, u_n, u_nnz , Int(0), Int(u_n),
+                v_idx, v_val, Scal(1)  , u_n, Int(0), Int(0), Int(u_n)
+            );
         }
         
     } // namespace Sparse
@@ -321,10 +437,30 @@ namespace Tensors
         return Sparse::VectorPartialDot(
             u.Indices().data(),
             u.Values().data(),
-            u.Size(), u.NonzeroCount(), u_i_begin, u_i_end,
+            u.DefaultValue(),
+            u.Size(), u.ExplicitCount(), u_i_begin, u_i_end,
             v.Indices().data(),
             v.Values().data(),
-            v.Size(), v.NonzeroCount(), v_i_begin, v_i_end
+            v.DefaultValue(),
+            v.Size(), v.ExplicitCount(), v_i_begin, v_i_end
+        );
+    }
+    
+    template<
+        typename u_Scal, typename Int,
+        typename Scal = u_Scal
+    >
+    Scal PartialSum(
+        cref<Sparse::Vector<u_Scal,Int>> u,
+        const Int  u_i_begin,
+        const Int  u_i_end
+    )
+    {
+        return Sparse::VectorPartialSum<u_Scal,Int,Scal>(
+            u.Indices().data(),
+            u.Values().data(),
+            u.DefaultValue(),
+            u.Size(), u.ExplicitCount(), u_i_begin, u_i_end
         );
     }
     
@@ -333,7 +469,7 @@ namespace Tensors
         typename v_Scal, typename v_Int,
         typename Scal = decltype( u_Scal(0) * v_Scal(0) )
     >
-    Scal PartialDot(
+    Scal Dot(
         cref<Sparse::Vector<u_Scal,u_Int>> u,
         cref<Sparse::Vector<v_Scal,v_Int>> v
     )
@@ -341,10 +477,28 @@ namespace Tensors
         return Sparse::template VectorPartialDot<u_Scal,u_Int,v_Scal,v_Int,Scal>(
             u.Indices().data(),
             u.Values().data(),
-            u.Size(), u.NonzeroCount(), u_Int(0), u.Size(),
+            u.DefaultValue(),
+            u.Size(), u.ExplicitCount(), u_Int(0), u.Size(),
             v.Indices().data(),
             v.Values().data(),
-            v.Size(), v.NonzeroCount(), v_Int(0), v.Size()
+            v.DefaultValue(),
+            v.Size(), v.ExplicitCount(), v_Int(0), v.Size()
+        );
+    }
+    
+    template<
+        typename u_Scal, typename Int,
+        typename Scal = u_Scal
+    >
+    Scal Total(
+        cref<Sparse::Vector<u_Scal,Int>> u
+    )
+    {
+        return Sparse::VectorTotal<u_Scal,Int,Scal>(
+            u.Indices().data(),
+            u.Values().data(),
+            u.DefaultValue(),
+            u.Size(), u.ExplicitCount()
         );
     }
     
